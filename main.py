78b008307c50de0679f6f9887ab6638d5603dedd
pierelucas <pierelucas@keemail.me>
2019-12-12 16:56:07 +0100

    main

--- main.py
+++ main.py
@@ -2,14 +2,38 @@ __autor__ = "Julian Huch"
 __version__ = "1.0"
 
 
-from exifreader import *
+import sys
+from exifreader import ImageHandler
 
 
 class Main():
+    """ Main Class """
+
+    menu_txt = """
+    [1] Download Pictures from URL
+    [2] Read exif from file
+    [0] Exit
+    """
 
     @staticmethod
     def run():
-        pass
+
+        print(Main.menu_txt)
+        choice = input("Choice > ")
+
+        if choice == 1:
+            image_url = input("Image URL > ")
+            ImageHandler.download_images_from_html(image_url)
+
+        elif choice == 2:
+            image_file = input("Image file > ") 
+            ImageHandler.print_exif_from_files(image_file)
+
+        elif choice == 0:
+            sys.exit(0)
+
+        else:
+            Main.run()
 
 
 if __name__ == "__main__":
