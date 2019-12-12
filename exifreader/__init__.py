376d61700368d4804ce70a461ea7263e11ca8d8c
pierelucas <pierelucas@keemail.me>
2019-12-12 16:40:58 +0100

    git init

--- exifreader/__init__.py
+++ exifreader/__init__.py
@@ -4,7 +4,6 @@ __versin__ = "1.0"
 
 
 import os
-import sys
 import exif
 import exifread
 from collections import defaultdict
@@ -13,16 +12,17 @@ import urllib
 from bs4 import BeautifulSoup
 
 
-#from exifreader.reader_error import NoExifError, WebsiteDownError, NoImageError
+from exifreader.reader_error import NoExifError, WebsiteDownError, NoImageError
 
 
 class BaseImageHandler(object):
     """ Base Class for handle images. """
+
     def __repr__(self):
         return f"{self.__class__.__name__!r}"
-    def print_exif_from_file(self):
+    def print_exif_from_files(self):
         pass
-    def return_exifdict_from_file(self):
+    def return_exiflist_from_files(self):
         pass
     def mod_exif_in_file(self):
         pass
@@ -31,57 +31,65 @@ class BaseImageHandler(object):
 
 
 class ImageHandler(BaseImageHandler):
+    """ Image Handler class, contains some static methods for handling exif data """
 
     def __repr__(self):
         return f"{self.__class__.__name__!r}"
 
     @staticmethod
-    def print_exif_from_file(image_file):
+    def print_exif_from_files(*args):
+        
+        for image_file in args:
 
-        try:
-            if not os.path.isfile(image_file):
-                raise FileNotFoundError(image_file)
-        
-            with open(image_file, "rb") as f:
-                if exif.Image(f).has_exif:
-                    tags = exifread.process_file(f)
-                    for key, value in tags.items():
-                        print(f"{key}: {value}")
-                else:
-                    raise NoExifError(image_file)
-        
-        except FileNotFoundError as ex:
-            print(ex)
-            sys.exit(1)
-        except NoExifError as ex:
-            print(ex)
-            sys.exit(1)
-        except Exception as ex:
-            print(ex)
-            sys.exit(1)
+            try:
+                # Check if the file exists, otherwise raise FileNotFoundError.
+                if not os.path.isfile(image_file):
+                    raise FileNotFoundError(image_file)
+                
+                # Open the file, check if there are exif data, otherwise raise NoExifError.
+                with open(image_file, "rb") as f:
+                    if exif.Image(f).has_exif:
+                        tags = exifread.process_file(f)     # this returns a dictionary contains exif data.
+                        for key, value in tags.items():
+                            print(image_file)
+                            print(f"{key}: {value}")        # print the exif data from the dictionary.
+                    else:
+                        raise NoExifError(image_file)
+            
+            except FileNotFoundError as ex:
+                print(ex)
+            except NoExifError as ex:
+                print(ex)
+            except Exception as ex:
+                print(ex)
     
     @staticmethod
-    def return_exifdict_from_file(image_file):
+    def return_exiflist_from_files(*args):
 
-        try:
-            if not os.path.isfile(image_file):
-                raise FileNotFoundError(image_file)
+        mret = []
+
+        for image_file in args:
+
+            try:
+                # Check if the file exists, otherwise raise FileNotFoundError.
+                if not os.path.isfile(image_file):
+                    raise FileNotFoundError(image_file)
+                
+                # Open the file, check if there are exif data, otherwise raise NoExifError.
+                with open(image_file, "rb") as f:
+                    if exif.Image(f).has_exif:
+                        mret.append(exifread.process_file(f))    # adds dictionary to mret list.
+                    else:
+                        raise NoExifError(image_file)
+            
+            except FileNotFoundError as ex:
+                print(ex)
+            except NoExifError as ex:
+                print(ex)
+            except Exception as ex:
+                print(ex)
         
-            with open(image_file, "rb") as f:
-                if exif.Image(f).has_exif:
-                    return exifread.process_file(f)
-                else:
-                    raise NoExifError(image_file)
-        
-        except FileNotFoundError as ex:
-            print(ex)
-            sys.exit(1)
-        except NoExifError as ex:
-            print(ex)
-            sys.exit(1)
-        except Exception as ex:
-            print(ex)
-            sys.exit(1)
+        return mret     # returns list with dictionay who contains exif data
     
     @staticmethod
     def mod_exif_from_file(image_file):
@@ -95,8 +103,8 @@ class ImageHandler(BaseImageHandler):
             # Check if the site's up, otherwise raise WebsiteDownError.
             if response.status_code == 200:
 
-                soup = BeautifulSoup(response.text, "html.parser")  # Parse html code from website
-                images = soup.find_all("img")   # Filter image tag out of parsed html code
+                soup = BeautifulSoup(response.text, "html.parser")      # Parse html code from website
+                images = soup.find_all("img")                           # Filter image tag out of parsed html code
 
                 # Check if there are images and create a directory, otherwise raise NoImageError.
                 if images:
@@ -125,13 +133,10 @@ class ImageHandler(BaseImageHandler):
         
         except WebsiteDownError as ex:
             print(ex)
-            sys.exit(1)
         except NoImageError as ex:
             print(ex)
-            sys.exit(1)
         except Exception as ex:
             print(ex)
-            sys.exit(1)
 
 
 if __name__ == "__main__":
---
+++ main.py
@@ -0,0 +1,16 @@
+__autor__ = "Julian Huch"
+__version__ = "1.0"
+
+
+from exifreader import *
+
+
+class Main():
+
+    @staticmethod
+    def run():
+        pass
+
+
+if __name__ == "__main__":
+    Main.run()
