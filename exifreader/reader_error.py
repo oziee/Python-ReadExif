33e5bd3aba232d52b73c513e82f340b9e6cf78d9
pierelucas <pierelucas@keemail.me>
2019-12-13 14:40:45 +0100

    adding FileNotFoundError wrapper

--- exifreader/reader_error.py
+++ exifreader/reader_error.py
@@ -2,6 +2,9 @@ __autor__ = "Julian Huch"
 __version__ = "1.0"
 
 
+from colorama import Fore, Style
+
+
 class BaseError(Exception):
     """ Base class for exceptions in this module. """
     def __repr__(self):
@@ -13,7 +16,7 @@ class NoExifError(BaseError):
     def __init__(self, value):
         self.value = value
     def __repr__(self):
-        return f"NoExifError: {self.value!r}"
+        return Fore.RED + f"NoExifError: {self.value!r}" + Style.RESET_ALL
     def __str__(self):
         return self.__repr__()
 
@@ -23,7 +26,7 @@ class WebsiteDownError(BaseError):
     def __init__(self, value):
         self.value = value
     def __repr__(self):
-        return f"WebsiteDownError: {self.value!r}"
+        return Fore.RED + f"WebsiteDownError: {self.value!r}" + Style.RESET_ALL
     def __str__(self):
         return self.__repr__()
 
@@ -33,6 +36,16 @@ class NoImageError(BaseError):
     def __init__(self, value):
         self.value = value
     def __repr__(self):
-        return f"NoImageError: {self.value!r}"
+        return Fore.RED + f"NoImageError: {self.value!r}" + Style.RESET_ALL
+    def __str__(self):
+        return self.__repr__()
+
+
+class FileNotFoundError(BaseError):
+    """ Raised when the file or directory didn't exist. """
+    def __init__(self, value):
+        self.value = value
+    def __repr__(self):
+        return Fore.RED + f"No such file or directory: {self.value!r}" + Style.RESET_ALL
     def __str__(self):
         return self.__repr__()
